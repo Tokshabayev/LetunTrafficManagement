c486b0099077446f8affb98bcc6c5ae4284dde67
bumeranger <raiymbek@gmail.com>
2025-05-24 11:13:24 +0500

    auth admin

--- letun-api/core/routes/drones_route.go
+++ letun-api/core/routes/drones_route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"letun-api/core/handlers"
+	"letun-api/core/middlewares"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -9,11 +10,11 @@ import (
 func InitDronesRoute(r *chi.Mux) {
 	dronesHandler := handlers.DronesHandler{}
 	r.Route("/drones", func(r chi.Router) {
-		r.Post("/", dronesHandler.Create)
-		r.Get("/{id}", dronesHandler.GetById)
-		r.Get("/", dronesHandler.List)
-		r.Post("/{id}/block", dronesHandler.Block)
-		r.Post("/{id}/unblock", dronesHandler.Unblock)
-		r.Post("/{id}/update", dronesHandler.Update)
+		r.Post("/", middlewares.AuthRequired(dronesHandler.Create, "admin"))
+		r.Get("/{id}", middlewares.AuthRequired(dronesHandler.GetById, "admin"))
+		r.Get("/", middlewares.AuthRequired(dronesHandler.List, "admin"))
+		r.Post("/{id}/block", middlewares.AuthRequired(dronesHandler.Block, "admin"))
+		r.Post("/{id}/unblock", middlewares.AuthRequired(dronesHandler.Unblock, "admin"))
+		r.Post("/{id}/update", middlewares.AuthRequired(dronesHandler.Update, "admin"))
 	})
 }
